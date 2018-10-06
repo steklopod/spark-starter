397009266b432bf0cc884885170e6ec4329df7e2
steklopod <steklopod@gmail.com>
2018-10-06 16:37:14 +0300

    Junit 5 test

--- src/test/scala/ru/example/InitTest.scala
+++ src/test/scala/ru/example/InitTest.scala
@@ -5,10 +5,12 @@ import org.scalatest.{FunSpec, FunSuite, Matchers}
 class InitTest extends FunSpec with Matchers {
 
   describe("running a test") {
+
     println("0. in describe")
 
     it("should do something") {
       println("2. in my test")
     }
   }
+
 }
--- src/test/scala/ru/example/Junit5Test.scala
+++ src/test/scala/ru/example/Junit5Test.scala
@@ -1,12 +1,13 @@
 package ru.example
 
 import org.junit.jupiter.api.{DisplayName, Test}
+import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
-import org.scalatest.junit.{JUnitRunner, JUnitSuite}
 import org.scalatest.{BeforeAndAfterAll, Matchers}
+import org.scalatest.junit.JUnitSuite
 
-@RunWith(classOf[JUnitRunner])
-class Junit5Test extends JUnitSuite with Matchers with BeforeAndAfterAll {
+@RunWith(classOf[JUnitPlatform])
+class Junit5Test  extends JUnitSuite with Matchers with BeforeAndAfterAll {
   //http://www.scalatest.org/user_guide/using_junit_runner
 
   override def beforeAll: Unit = { println(">>> beforeAll <<< ") }
